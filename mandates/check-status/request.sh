aabe76d0e3aee420e2198cf00a9d2031aa5cf8c7
Rubai Mandal <rubai.mandal@juspay.in>
2024-03-15 15:12:20 +0530

    change: github>juspay>juspay-sdk-integration-demo>api-reference>mandates/check-status/request.sh @ hyper-checkout/web/mandate-execution-auto-debit-of-subscription by Rubai Mandal

--- mandates/check-status/request.sh
+++ mandates/check-status/request.sh
@@ -2,3 +2,4 @@ curl --location 'https://sandbox.juspay.in/mandates/hyhTD5Ga4jJ2KXZwwnyh4A' \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Authorization: your api key' \
 --data-urlencode 'command=check_status'
+test
